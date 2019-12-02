ed5df11b18be5f37ec91c606e1c0a140c86a0a95
Mike Neilens <mike.neilens@gmail.com>
2019-12-02 20:24:05 +0000

    day two part two

--- day02/src/main/kotlin/Main.kt
+++ day02/src/main/kotlin/Main.kt
@@ -1,7 +1,5 @@
 fun process(sampleData: List<Int>): List<Int> {
     val result = sampleData.toMutableList()
-
-
     for (index in (0..(result.size - 1)) step 4) {
         val opcode = result[index]
         when (opcode) {
@@ -11,4 +9,17 @@ fun process(sampleData: List<Int>): List<Int> {
         }
     }
     return result
+}
+
+fun findInputsThatCreateAValue(sampleData:List<Int>, valueToFind:Int):Pair<Int,Int> {
+    for (parameter1 in 0..99) {
+        for (parameter2 in 0..99) {
+            val result = sampleData.toMutableList()
+            result[1] = parameter1
+            result[2] = parameter2
+            val resultOfProcess = process(result)
+            if (resultOfProcess[0] == valueToFind) return Pair(parameter1,parameter2)
+        }
+    }
+    return Pair(0,0)
 }
