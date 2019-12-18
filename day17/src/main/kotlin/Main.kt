2ba847a2cff1ea63b126afcc52c7cc47fcb6f8cf
Mike Neilens <mike.neilens@gmail.com>
2019-12-18 21:11:44 +0000

    day 17 part two nearly there

--- day17/src/main/kotlin/Main.kt
+++ day17/src/main/kotlin/Main.kt
@@ -1,19 +1,34 @@
-import javafx.geometry.Pos
-
 data class Position(val x:Int, val y:Int) {
     operator fun plus(other:Position):Position = Position(this.x + other.x, this.y + other.y)
 }
 enum class Content(val value:Char) {
     Empty ('.'),
     Scaffold('#'),
-    Intersection('O');
+    Intersection('O'),
+    Robot('^');
     companion object {
-        fun create(value:Char):Content {
-            return when (value) {
-                '#' -> Scaffold
-                else -> Empty
-            }
-        }
+        fun create(value:Char) = values().filter { it.value == value }.firstOrNull() ?: Empty
+    }
+}
+
+typealias ScaffoldMap = MutableMap<Position, Content>
+
+enum class Direction(val move:Position) {
+    Up(Position(0,-1)),
+    Down(Position(0,1)),
+    Left(Position(-1,0)),
+    Right(Position(1,0));
+    fun turnLeft() = when(this) {
+        Up -> Left
+        Left -> Down
+        Down -> Right
+        Right -> Up
+    }
+    fun turnRight() = when(this) {
+        Up -> Right
+        Right -> Down
+        Down -> Left
+        Left -> Up
     }
 }
 
@@ -24,12 +39,6 @@ fun getScaffold(sourceCode:List<Number>):Map<Position, Content> {
     return convertTextToMap(scaffoldAsText)
 }
 
-fun printScaffold(scaffoldMap:Map<Position, Content>) {
-    val xRange = (scaffoldMap.keys.map{it.x}.min() ?: 0)..(scaffoldMap.keys.map{it.x}.max() ?: 0)
-    val yRange = (scaffoldMap.keys.map{it.y}.min() ?: 0)..(scaffoldMap.keys.map{it.y}.max() ?: 0)
-    yRange.forEach{y -> xRange.forEach { x -> print(scaffoldMap[Position(x,y)]?.value) }
-        println() }
-}
 fun convertTextToMap(scaffoldText:String):Map<Position, Content> {
     val scaffoldMap = mutableMapOf<Position,Content>()
     val rows = scaffoldText.split("\n")
@@ -41,12 +50,78 @@ fun convertTextToMap(scaffoldText:String):Map<Position, Content> {
     return scaffoldMap
 }
 
-fun updateScaffoldIntersections(scaffoldMap: MutableMap<Position, Content>):Int {
-    val surroundingPositions = listOf(Position(-1,0),Position(0,-1),Position(1,0),Position(0,1))
-    scaffoldMap.forEach { position, content ->
-        if ((content == Content.Scaffold) && surroundingPositions.all{scaffoldMap[position + it] == Content.Scaffold}) {
-            scaffoldMap[position] = Content.Intersection
+fun ScaffoldMap.xRange() = (keys.map{it.x}.min() ?: 0)..(keys.map{it.x}.max() ?: 0)
+fun ScaffoldMap.yRange() = (keys.map{it.y}.min() ?: 0)..(keys.map{it.y}.max() ?: 0)
+
+fun ScaffoldMap.print() {
+    val xRange = xRange()
+    val yRange = yRange()
+    yRange.forEach{y -> xRange.forEach { x -> print(this[Position(x,y)]?.value) }
+        println() }
+}
+
+fun ScaffoldMap.updateIntersections():Int {
+    val surroundingPositions = Direction.values().map{it.move}
+    forEach { position, content ->
+        if ((content == Content.Scaffold) && surroundingPositions.all{this[position + it] == Content.Scaffold}) {
+            this[position] = Content.Intersection
         }
     }
-    return scaffoldMap.filter{it.value == Content.Intersection}.keys.fold(0){ a, position -> a + (position.x * position.y) }
+    return filter{it.value == Content.Intersection}.keys.fold(0){ a, position -> a + (position.x * position.y) }
 }
+
+fun ScaffoldMap.findRobot() = filter{it.value == Content.Robot}.keys.first()
+fun ScaffoldMap.canMove(position:Position, direction:Direction) = this[position + direction.move] == Content.Scaffold
+fun ScaffoldMap.canTurnLeft(position:Position, direction:Direction) = this[position + direction.turnLeft().move] == Content.Scaffold
+fun ScaffoldMap.canTurnRight(position:Position, direction:Direction) = this[position + direction.turnRight().move] == Content.Scaffold
+
+fun ScaffoldMap.getMoves():List<Pair<String,Int>> {
+    var robotPosition = findRobot()
+    var robotDirection = Direction.Up
+    val path = mutableListOf<Pair<String,Int>>()
+
+    while (canMove(robotPosition, robotDirection) || canTurnLeft(robotPosition,robotDirection) || canTurnRight(robotPosition,robotDirection)) {
+        var steps = 0
+        var lastTurn= ""
+
+        if (!canMove(robotPosition, robotDirection)) {
+            lastTurn = when (true) {
+                canTurnLeft(robotPosition,robotDirection) -> "L"
+                canTurnRight(robotPosition,robotDirection) -> "R"
+                else -> ""
+            }
+            robotDirection = when (true) {
+                canTurnLeft(robotPosition,robotDirection) -> robotDirection.turnLeft()
+                canTurnRight(robotPosition,robotDirection) -> robotDirection.turnRight()
+                else -> robotDirection
+            }
+        }
+
+        while (canMove(robotPosition, robotDirection)) {
+            robotPosition = robotPosition + robotDirection.move
+            steps = steps + 1
+        }
+        path.add(Pair(lastTurn,steps))
+        println("Turn: $lastTurn $steps")
+    }
+    return path
+}
+fun List<Pair<String,Int>>.findRepeatingSequence(size:Int = this.size):List<Pair<String,Int>>?{
+    val size = if (size < this.size) size else this.size
+    if (size < 1) return null
+    val dataChunked = this.chunked(size)
+    return dataChunked[0]
+}
+fun List<Pair<String,Int>>.removeRepeatingSequence(repeatingSequence:List<Pair<String,Int>>?):List<Pair<String,Int>>?{
+    if (repeatingSequence == null) return null
+    var newSequence = mutableListOf<Pair<String,Int>>()
+    val dataChunked = this.chunked(repeatingSequence.size)
+    for (chunk in dataChunked) {
+        if (chunk != repeatingSequence) {
+            newSequence.addAll(chunk)
+        }
+    }
+    return newSequence
+}
+
+
